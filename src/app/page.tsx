123cf74ae619cf44c991e396aeb5ae3c0a74938b
codewithpandu <pandusetiadarmawan01@gmail.com>
2026-05-18 07:53:31 +0700

    add: service section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,16 @@ import { Button } from "@/components/ui/button";
 import HeroImage from "../../public/img/hero.jpg";
 import Image from "next/image";
 import AppSection from "@/components/AppSection";
-import { MoveRight } from "lucide-react";
+import { Building2, Compass, MoveRight } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -78,22 +87,96 @@ export default function Home() {
         </div>
       </AppSection>
 
-      <AppSection id="about" className="bg-my-secondary py-16 px-4 ">
-        <small className="text-amber-500 tracking-widest">TENTANG KAMI</small>
-        <div className="md:flex py-2 space-y-2">
+      <AppSection id="service" className="bg-my-primary py-16 px-4 ">
+        <small className="text-amber-500 tracking-widest">LAYANAN</small>
+        <div className="md:flex py-2 space-y-2 text-white">
           <h2 className="font-serif font-bold text-3xl md:w-1/2">
-            Solusi Modern untuk Pertumbuhan Berkelanjutan
+            Inovasi untuk Masa Depan
           </h2>
           <p className="md:w-1/2">
-            Kami adalah tim arsitek, desainer, dan ahli strategi yang percaya
-            bahwa setiap perusahaan layak memiliki ruang dan identitas yang
-            menggerakkan orang.
-            <br />
-            <br />
-            Selama satu dekade, kami menggabungkan kerajinan tradisional dengan
-            pemikiran modern untuk menciptakan pengalaman yang tahan terhadap
-            waktu.
+            Kami mendukung individu maupun perusahaan dalam membangun identitas,
+            mengembangkan sistem, dan menghadirkan karya berkualitas tinggi.
           </p>
+        </div>
+        <div className="py-16 flex flex-col lg:flex-row gap-4">
+          <div className="w-full lg:max-w-md">
+            <Card className="bg-my-secondary">
+              <CardHeader>
+                <CardTitle>
+                  <Building2 className="size-12" />
+                </CardTitle>
+                <CardDescription className="font-bold text-lg text-black">
+                  Arsitektur
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Ruang kerja dan komersial yang menyeimbangkan estetika dengan
+                  fungsi.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 hover:text-amber-500"
+                >
+                  Pelajari lebih <MoveRight className="w-4" />
+                </Link>
+              </CardFooter>
+            </Card>
+          </div>
+          <div className="w-full lg:max-w-md">
+            <Card className="bg-my-secondary">
+              <CardHeader>
+                <CardTitle>
+                  <Compass className="size-12" />
+                </CardTitle>
+                <CardDescription className="font-bold text-lg text-black">
+                  Brand & Identitas
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Sistem visual yang membuat perusahaan Anda mudah dikenali dan
+                  dipercaya.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 hover:text-amber-500"
+                >
+                  Pelajari lebih <MoveRight className="w-4" />
+                </Link>
+              </CardFooter>
+            </Card>
+          </div>
+          <div className="w-full lg:max-w-md">
+            <Card className="bg-my-secondary">
+              <CardHeader>
+                <CardTitle>
+                  <Building2 className="size-12" />
+                </CardTitle>
+                <CardDescription className="font-bold text-lg text-black">
+                  Arsitektur
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Ruang kerja dan komersial yang menyeimbangkan estetika dengan
+                  fungsi.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 hover:text-amber-500"
+                >
+                  Pelajari lebih <MoveRight className="w-4" />
+                </Link>
+              </CardFooter>
+            </Card>
+          </div>
         </div>
       </AppSection>
     </div>
