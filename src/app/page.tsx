47b03e27031f8d137feb0467850f6f31036d9d8c
codewithpandu <pandusetiadarmawan01@gmail.com>
2026-05-18 20:22:01 +0700

    fix: image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,11 +24,11 @@ export default function Home() {
           <Image
             src={HeroImage}
             alt="Hero"
-            className="w-full h-245 md:h-340 object-cover"
+            className="w-full h-245 md:h-340 2xl:h-full object-cover"
           />
           <div className="absolute top-0 left-0 right-0 bg-sky-900/80 w-full h-full"></div>
         </div>
-        <div className="absolute top-60 md:top-1/2 md:-translate-y-70 space-y-2 md:space-y-4">
+        <div className="absolute top-60 md:top-1/2 md:-translate-y-70 2xl: space-y-2 md:space-y-4">
           <h1 className="text-5xl md:text-7xl xl:text-8xl font-bold font-serif text-white ms-4 md:w-170 lg:w-220">
             Membangun visi, <span className="text-amber-500">mendesain</span>{" "}
             masa depan.
